fc191c4e138da5785493f668a3162325d3982195
Simone-Westphal <sw@thinkweb360.de>
2020-10-29 19:02:10 +0100

    chore: package.json

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Movies from './components/movies';
 
 class App extends Component {
   render() {
     return (
       <main className="container">
-          <h1>huhu</h1>
+          <Movies/>
       </main>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import  'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
+import App from './App.js';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
